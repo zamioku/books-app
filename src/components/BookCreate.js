1270324698cab5ce3043cb500ff49471aae90ebe
Sorin Dragomir <sorin.dragomir90@gmail.com>
2024-02-12 19:44:31 +0200

    first version

---
+++ src/components/BookCreate.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle(""); //clear input field
+  };
+
+  return (
+    <div className="book-create">
+      <h3>Add a book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Add book</button>
+      </form>
+    </div>
+  );
+}
+
+export default BookCreate;
